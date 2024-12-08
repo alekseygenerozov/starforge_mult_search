a42eb09f9c4512e25fe205e3fb645fcadc9c1a75
Aleksey Generozov <aleksey.generozov@gmail.com>
2024-12-08 10:36:20 +0200

    sink_data modifications

---
+++ code/run_batch_sink.py
@@ -0,0 +1,21 @@
+import sys
+import subprocess
+import glob
+
+def bash_command(cmd, **kwargs):
+        '''Run command from the bash shell'''
+        process=subprocess.Popen(['/bin/bash', '-c',cmd],  **kwargs)
+        return process.communicate()[0]
+
+with open("data_loc", "r") as ff:
+	snap_base = ff.read().strip()
+snaps = glob.glob(snap_base + "*hdf5")
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+if end < 0:
+	end = len(snaps) - 1
+for ii in range(start, end + 1, 1):
+        snap = f"{snap_base}_{ii}.hdf5"
+        cmd = f"python3 sink_data.py {snap}"
+        bash_command(cmd)
