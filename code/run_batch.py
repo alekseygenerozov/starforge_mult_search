f1370e4d5bce3238be207e3ec16afffa50904ace
alekseygenerozov <aleksey.generozov@gmail.com>
2025-01-20 15:13:44 -0600

    Parallelizing run_batch.py

--- code/run_batch.py
+++ code/run_batch.py
@@ -2,10 +2,16 @@ import sys
 import subprocess
 import glob
 
+import multiprocessing
+
 def bash_command(cmd, **kwargs):
         '''Run command from the bash shell'''
         process=subprocess.Popen(['/bin/bash', '-c',cmd],  **kwargs)
         return process.communicate()[0]
+
+def run_find_bins(ii, snap_base, tides_factor):
+    cmd = f"python3 find_multiples_new2.py {ii} --halo_mass_file halo_masses/M2e4halo_masses_sing_npTrue_c0.5 --ngrid 1 --snap_base {snap_base} --tides_factor {tides_factor}"
+    bash_command(cmd)
 
 
 with open("data_loc", "r") as ff:
@@ -16,7 +22,6 @@ start = int(sys.argv[1])
 end = int(sys.argv[2])
 if end < 0:
 	end = len(snaps) - 1
-for ii in range(start, end + 1, 1):
-        cmd = f"python3 find_multiples_new2.py {ii} --halo_mass_file halo_masses/M2e4halo_masses_sing_npTrue_c0.5 --ngrid 1 --snap_base {snap_base} --tides_factor {sys.argv[3]}"
-        print(cmd)
-        bash_command(cmd)
+
+with multiprocessing.Pool(56) as pool:
+    pool.map(run_find_bins, [(ii, snap_base, tides_factor) for ii in range(start, end + 1)])
