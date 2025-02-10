34b5f61c3ec5c905e5fcd213956f907bc94da321
alekseygenerozov <aleksey.generozov@gmail.com>
2025-02-10 16:54:07 -0600

    more paths

--- analysis/pretty_halo.py
+++ analysis/pretty_halo.py
@@ -180,7 +180,7 @@ with open(base + "/data_loc", "r") as ff:
 r2 = f"_TidesTrue_smaoFalse_mult4_ngrid1_hmTrue_ft{my_ft}_coFalse.p".replace(".p", "")
 aa = "analyze_multiples_output_" + r2 + "/"
 
-snap_file = snap_base + f"snapshot_{snap_idx:03d}.hdf5"
+snap_file = snap_base + f"_{snap_idx:03d}.hdf5"
 den, x, m, h, u, b, v, fmol, fneu, partpos, partmasses, partvels, partids, partsink, tage_myr, unit_base = \
 find_multiples_new2.load_data(snap_file, res_limit=1e-3)
 ##WOULD HAVE BEEN BETTER TO PUT THIS FUNCTIONALITY IN LOAD DATA!!!
