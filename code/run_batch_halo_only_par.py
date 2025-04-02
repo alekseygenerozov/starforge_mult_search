a0d024f1f69040c7db094b862bb4bd5e6271eba8
Aleksey Generozov <aleksey.generozov@gmail.com>
2025-04-02 20:54:22 +0000

    Edited run_batch.py, run_batch_halo_only_par.py

--- code/run_batch_halo_only_par.py
+++ code/run_batch_halo_only_par.py
@@ -21,6 +21,6 @@ end = int(sys.argv[2])
 if end < 0:
 	end = (len(snaps) - 1) * cadence
 for ii in range(start, end + 1, cadence):
-	my_cmd = f"python3 starforge_mult_search/code/halo_masses_single_double_par.py --non_pair --tides_factor {sys.argv[3]} --snap_base {snap_base}  {ii}"
+	my_cmd = f"python3 halo_masses_single_double_par.py --non_pair --tides_factor {sys.argv[3]} --snap_base {snap_base}  {ii}"
 	print(my_cmd)
 	bash_command(my_cmd)
