06bfbb6fecbd5cc0174d10fbf3b9fa0ea6d65eae
alekseygenerozov <aleksey.generozov@gmail.com>
2025-04-02 20:17:33 -0500

    Fixing legend

--- code/analysis/figures/ex_fig1.py
+++ code/analysis/figures/ex_fig1.py
@@ -33,6 +33,7 @@ ax.errorbar(bins_center, n1 / d1, \
             yerr=n1u / d1, marker="s", linestyle="", alpha=0.7, label="Survivors")
 ax.errorbar(bins_center, n2 / d2, \
             yerr=n2u / d2, marker="s", linestyle="", alpha=0.7, label="Non-survivors")
+ax.legend(loc="lower left")
 fig.savefig("ex_fig1.pdf")
 
 fig, ax = plt.subplots()
@@ -42,9 +43,8 @@ ax.set_xlabel(r"$log(M_{prim, f} / M_{\odot})$")
 ax.set_ylabel("BFB Fraction")
 
 ax.errorbar(bins_center, n3 / d3, \
-            yerr=n3u / d3, marker="s", linestyle="", alpha=0.7, label="Survivors")
+            yerr=n3u / d3, marker="s", linestyle="", alpha=0.7)
 
 print(n3 /d3)
 # ax.legend(title=r"$f_t=$"+f"{my_ft}")
-ax.legend(loc="lower left")
 fig.savefig("fig2b.pdf")
