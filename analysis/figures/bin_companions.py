c2c27890f535c52f58a1d0ce25467024e1bc5b1c
alekseygenerozov <aleksey.generozov@gmail.com>
2025-04-28 15:36:47 -0500

    Tweaks to companions script

--- analysis/figures/bin_companions.py
+++ analysis/figures/bin_companions.py
@@ -12,47 +12,35 @@ tval = high_df.index.get_level_values("t")
 high_df["tval"] = tval
 high_df = high_df.loc[(high_df["frac_of_orbit_seg"] >= 1) & (high_df["nbound_snaps_seg"] > 1)]
 bin_ids = my_data["bin_ids"]
+quasi_filter = my_data["quasi_filter"]
 
-comps_a_ids = []
-comps_a_times = []
-comps_a_ids_flat = []
+comps_a_ids = [[]  for _ in range(len(bin_ids))]
+comps_a_times = [[]  for _ in range(len(bin_ids))]
+comps_a_ids_flat = [[]  for _ in range(len(bin_ids))]
 
-comps_b_ids = []
-comps_b_times = []
-comps_b_ids_flat = []
-for row in tqdm.tqdm(bin_ids):
+comps_b_ids = [[]  for _ in range(len(bin_ids))]
+comps_b_times = [[]  for _ in range(len(bin_ids))]
+comps_b_ids_flat = [[]  for _ in range(len(bin_ids))]
+for ii, row in tqdm.tqdm(enumerate(bin_ids)):
     bin_list = list(row)
+    if not quasi_filter[ii]:
+        continue
 
     mult_ids_list = high_df["mult_ids_list"]
     tmp_sel2a = high_df.loc[high_df.index.get_level_values("id").str.contains(rf"\b{bin_list[0]}\b")]
     tmp_sel2b = high_df.loc[high_df.index.get_level_values("id").str.contains(rf"\b{bin_list[1]}\b")]
 
-    comps_a_ids = []
-    comps_a_times = []
-    comps_a_ids_flat = []
-
-    comps_b_ids = []
-    comps_b_times = []
-    comps_b_ids_flat = []
-
     if len(tmp_sel2a) > 0:
         comps_a = tmp_sel2a.groupby("t")[["tval", "mult", "mult_ids_list"]].apply(lambda g: g[g["mult"] == g["mult"].max()])
-        comps_a_ids.append(comps_a.index.get_level_values("id").to_numpy())
-        comps_a_times.append(comps_a["tval"].values)
-        comps_a_ids_flat.append(comps_a["mult_ids_list"].values)
-    else:
-        comps_a_ids.append([])
-        comps_a_times.append([])
-        comps_a_ids_flat.append([])
+        comps_a_ids[ii] = (comps_a.index.get_level_values("id").to_numpy())
+        comps_a_times[ii] = (comps_a["tval"].values)
+        comps_a_ids_flat[ii] = (comps_a["mult_ids_list"].values)
+
     if len(tmp_sel2b) > 0:
         comps_b = tmp_sel2b.groupby("t")[["tval", "mult", "mult_ids_list"]].apply(lambda g: g[g["mult"] == g["mult"].max()])
-        comps_b_ids.append(comps_b.index.get_level_values("id").to_numpy())
-        comps_b_times.append(comps_b["tval"].values)
-        comps_b_ids_flat.append(comps_b["mult_ids_list"].values)
-    else:
-        comps_b_ids.append([])
-        comps_b_times.append([])
-        comps_b_ids_flat.append([])
+        comps_b_ids[ii] = (comps_b.index.get_level_values("id").to_numpy())
+        comps_b_times[ii] = (comps_b["tval"].values)
+        comps_b_ids_flat[ii] = (comps_b["mult_ids_list"].values)
 
 
 np.savez("bin_persistent_comps.npz", comps_a_ids=comps_a_ids, comps_a_times=comps_a_times, comps_a_ids_flat=comps_a_ids_flat,
