936fbad6d852fdf0d85cc8caf577a13b88fca251
alekseygenerozov <aleksey.generozov@gmail.com>
2025-10-10 17:43:42 -0500

    Fixing hl_radius

--- analysis/figures/fig1_extend.py
+++ analysis/figures/fig1_extend.py
@@ -254,7 +254,7 @@ for ii in range(len(partpos_filt)):
     center_x, center_y = partpos_filt[ii, 0] - center[0], partpos_filt[ii, 1] - center[1]
     ax.plot(center_x, center_y, "kX", markersize= 4 * np.log(partmasses_filt[ii] / 0.001))
     # Create the circular patch comparable to the accretion radius...Really this is an upper bound(!)
-    hl_radius = 2. * sfc.GN * partmasses_filt[ii] / partvel_filt[ii]**2.
+    hl_radius = 2. * sfc.GN * partmasses_filt[ii] / np.linalg.norm(partvel_filt[ii])**2.
     circle = patches.Circle((center_x, center_y), hl_radius, color='red', fill=False, linewidth=2, label='Circle')
     # Add the circle to the axes
     ax.add_patch(circle)
