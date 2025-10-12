32195f8c5b3b7e58831fb802cfba9806e68040e0
alekseygenerozov <aleksey.generozov@gmail.com>
2025-10-12 18:35:41 -0500

    Debugging

--- analysis/figures/fig1_extend.py
+++ analysis/figures/fig1_extend.py
@@ -303,7 +303,7 @@ fig.savefig(f"fig1_{sys.argv[1]}b_{snap_idx}." + savetype, dpi=300)
 # prop_cycle = plt.rcParams['axes.prop_cycle']
 # colors = prop_cycle.by_key()['color']
 colors = ["w", "gold"]
-
+breakpoint()
 if tracer_file:
     tracer_data = np.genfromtxt(tracer_file)
     tracer_ids = tracer_data[:, 0]
