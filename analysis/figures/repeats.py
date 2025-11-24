c00e21c01d305aab716152c51a0ceb9e40445d72
alekseygenerozov <aleksey.generozov@gmail.com>
2025-11-24 15:54:08 -0600

    analysis/figures/repeats.py: getting repeats

--- analysis/figures/repeats.py
+++ analysis/figures/repeats.py
@@ -1,40 +1,52 @@
 import argparse
 import glob
+import subprocess
+
 import numpy as np
 import pandas as pd
-import subprocess
 
 from starforge_mult_search.code import find_multiples_new2
 
+
 def bash_command(cmd, **kwargs):
-    '''Run command from the bash shell'''
-    process = subprocess.Popen(['/bin/bash', '-c', cmd],  **kwargs)
+    """Run command from the bash shell"""
+    process = subprocess.Popen(["/bin/bash", "-c", cmd], **kwargs)
     return process.communicate()[0]
+
 
 def main():
     parser = argparse.ArgumentParser(description="Get repreating gas ids.")
-    parser.add_argument("--snap_base", default="snapshot", help="First part of snapshot name")
+    parser.add_argument(
+        "--snap_base", default="snapshot", help="First part of snapshot name"
+    )
     args = parser.parse_args()
 
     snaps = glob.glob(args.snap_base + "*hdf5")
-    start = 0 
-    end = len(snaps) 
+    start = 0
+    end = len(snaps)
 
     ##For collecting all the repeaters
     repeaters_all = []
     ##Iterate over all snapshots
-    for ss in range(399, 400):
+    for ss in range(start, end):
         print(ss)
-        snapshot_file = args.snap_base + '_{0:03d}.hdf5'.format(int(ss))
+        snapshot_file = args.snap_base + "_{0:03d}.hdf5".format(int(ss))
         ##Getting all the counts of all the ids.
         gas_ids = find_multiples_new2.load_gas_ids(snapshot_file, res_limit=1e-3)
         breakpoint()
         ##Getting the count
-        ordered_count = pd.Series(gas_ids).value_counts()#.loc[gas_ids].to_numpy()
-        gas_ids_repeat = np.transpose((ordered_count.loc[ordered_count>1].index, ordered_count.loc[ordered_count>1].values, [ss] * len(ordered_count.loc[ordered_count>1]))) 
+        ordered_count = pd.Series(gas_ids).value_counts()  # .loc[gas_ids].to_numpy()
+        gas_ids_repeat = np.transpose(
+            (
+                ordered_count.loc[ordered_count > 1].index,
+                ordered_count.loc[ordered_count > 1].values,
+                [ss] * len(ordered_count.loc[ordered_count > 1]),
+            )
+        )
         repeaters_all.append(gas_ids_repeat)
 
         np.savez("repeaters.npz", np.vstack(repeaters_all))
+
 
 if __name__ == "__main__":
     main()
