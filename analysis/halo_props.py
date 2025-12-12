43ecaf647e5a90ac631ffe0e9cc18003780d6455
alekseygenerozov <aleksey.generozov@gmail.com>
2025-12-12 11:21:37 -0600

    Getting all properties for halo_props.

--- analysis/halo_props.py
+++ analysis/halo_props.py
@@ -1,33 +1,54 @@
+import argparse
+import functools
+import multiprocessing
 import pickle
-import numpy as np
+import subprocess
 import sys
+import time
+
+import h5py
+import numpy as np
+
 ##Code uses functionality in find_multiples_new2
 # sys.path.append("/home/aleksey/Dropbox/projects/Hagai_projects/star_forge")
 import pytreegrav
-import argparse
-import h5py
-import multiprocessing
-import functools
-import time
-import subprocess
-
 from starforge_mult_search.code import find_multiples_new2
 
+
 def bash_command(cmd, **kwargs):
-    '''Run command from the bash shell'''
-    process = subprocess.Popen(['/bin/bash', '-c', cmd],  **kwargs)
+    """Run command from the bash shell"""
+    process = subprocess.Popen(["/bin/bash", "-c", cmd], **kwargs)
     return process.communicate()[0]
+
+
 def main():
     print("test")
-    parser = argparse.ArgumentParser(description="Parse starforge snapshot, and get multiple data.")
+    parser = argparse.ArgumentParser(
+        description="Parse starforge snapshot, and get multiple data."
+    )
     parser.add_argument("snap", help="Index of snapshot to read")
-    parser.add_argument("--snap_base", default="snapshot", help="First part of snapshot name")
-    parser.add_argument("--non_pair", action="store_true", help="Flag to turn on non-pairwise algorithm")
-    parser.add_argument("--compress", action="store_true", help="Filter out compressive tidal forces")
-    parser.add_argument("--tides_factor", type=float, default=8.0, help="Prefactor for check of tidal criterion (8.0)")
-    parser.add_argument("--cutoff", type=float, default=0.5, help="Outer cutoff to look for bound gas (0.5 pc)")
+    parser.add_argument(
+        "--snap_base", default="snapshot", help="First part of snapshot name"
+    )
+    parser.add_argument(
+        "--non_pair", action="store_true", help="Flag to turn on non-pairwise algorithm"
+    )
+    parser.add_argument(
+        "--compress", action="store_true", help="Filter out compressive tidal forces"
+    )
+    parser.add_argument(
+        "--tides_factor",
+        type=float,
+        default=8.0,
+        help="Prefactor for check of tidal criterion (8.0)",
+    )
+    parser.add_argument(
+        "--cutoff",
+        type=float,
+        default=0.5,
+        help="Outer cutoff to look for bound gas (0.5 pc)",
+    )
     parser.add_argument("--name_tag", default="M2e4", help="Extension for saving.")
-
 
     args = parser.parse_args()
     print(args)
@@ -37,11 +58,28 @@ def main():
     non_pair = args.non_pair
     name_tag = args.name_tag
 
-    snapshot_file = args.snap_base + '_{0:03d}.hdf5'.format(int(args.snap))
-    snapshot_num = f'{int(args.snap):03d}'
+    snapshot_file = args.snap_base + "_{0:03d}.hdf5".format(int(args.snap))
+    snapshot_num = f"{int(args.snap):03d}"
 
-    den, x, m, h, u, b, v, fmol, fneu, partpos, partmasses, partvels, partids, partsink, tage_myr, unit_base, partspin =\
-    find_multiples_new2.load_data(snapshot_file, res_limit=1e-3)
+    (
+        den,
+        x,
+        m,
+        h,
+        u,
+        b,
+        v,
+        fmol,
+        fneu,
+        partpos,
+        partmasses,
+        partvels,
+        partids,
+        partsink,
+        tage_myr,
+        unit_base,
+        partspin,
+    ) = find_multiples_new2.load_data(snapshot_file, res_limit=1e-3)
     halo_ids = find_multiples_new2.load_gas_ids(snapshot_file, res_limit=1e-3)
 
     xuniq, indx = np.unique(x, return_index=True, axis=0)
@@ -63,19 +101,37 @@ def main():
     partsink = partsink.astype(np.float64)
     halo_ids = halo_ids[indx]
 
-    halo_mass_name = "halo_masses_sing_np{0}_c{1}_{2}_comp{3}_tf{4}".format(non_pair, cutoff, snap_idx, args.compress,
-                                                                               args.tides_factor)
-    with h5py.File(halo_mass_name + ".hdf5", 'a') as gas_dat_h5:
+    halo_mass_name = "halo_masses_sing_np{0}_c{1}_{2}_comp{3}_tf{4}".format(
+        non_pair, cutoff, snap_idx, args.compress, args.tides_factor
+    )
+    with h5py.File(halo_mass_name + ".hdf5", "a") as gas_dat_h5:
         for ii in range(len(partids)):
             halo_idx = gas_dat_h5["halo_{0}".format(partids[ii])]
-            gas_dat_h5.create_dataset("halo_{0}_b".format(partids[ii]), data=buniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_pid".format(partids[ii]), data=halo_ids[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_h".format(partids[ii]), data=huniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_rho".format(partids[ii]), data=denuniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_x".format(partids[ii]), data=xuniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_v".format(partids[ii]), data=vuniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_u".format(partids[ii]), data=uuniq[halo_idx])
-            # gas_dat_h5.create_dataset("halo_{0}_m".format(partids[ii]), data=muniq[halo_idx])
+            gas_dat_h5.create_dataset(
+                "halo_{0}_b".format(partids[ii]), data=buniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_pid".format(partids[ii]), data=halo_ids[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_h".format(partids[ii]), data=huniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_rho".format(partids[ii]), data=denuniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_x".format(partids[ii]), data=xuniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_v".format(partids[ii]), data=vuniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_u".format(partids[ii]), data=uuniq[halo_idx]
+            )
+            gas_dat_h5.create_dataset(
+                "halo_{0}_m".format(partids[ii]), data=muniq[halo_idx]
+            )
+
 
 if __name__ == "__main__":
     main()
