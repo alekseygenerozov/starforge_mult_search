b7993af41b7a6a8c034b8aa38672625cf7ec1225
alekseygenerozov <aleksey.generozov@gmail.com>
2026-02-25 15:13:10 -0600

    halo_masses_single_double_select.py: Fixing snapshot filter for halo mask

--- code/halo_masses_single_double_select.py
+++ code/halo_masses_single_double_select.py
@@ -10,7 +10,6 @@ import time
 import h5py
 import numpy as np
 import pytreegrav
-
 import starforge_mult_search.code.starforge_constants as sfc
 
 ##Code uses functionality in find_multiples_new2
@@ -514,7 +513,7 @@ def main():
     ##Compute only subset of halo using a predefined mask(!)
     if args.halo_select:
         select = np.genfromtxt(args.halo_select).astype(int)
-        select = select[select[:, 0] == snap_idx]
+        select = select[select[:, 0] == int(snap_idx)]
         select = select[:, 1]
     else:
         select = partids
