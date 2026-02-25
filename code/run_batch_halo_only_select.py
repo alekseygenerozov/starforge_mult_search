79b203d21b21b0932000be6ac1d7c9c92c1d7254
alekseygenerozov <aleksey.generozov@gmail.com>
2026-02-25 16:08:59 -0600

    Fixing iteration over the halo snapshots.

--- code/run_batch_halo_only_select.py
+++ code/run_batch_halo_only_select.py
@@ -27,7 +27,7 @@ halo_dat = np.genfromtxt(sys.argv[-1]).astype(int)
 halo_dat = halo_dat[(halo_dat[:, 0] >= start) & (halo_dat[:, 0] <= end)]
 halo_snaps = np.unique(halo_dat[:, 0])
 
-for ii in range(halo_snaps):
+for ii in halo_snaps:
     my_cmd = f"python3 starforge_mult_search/code/halo_masses_single_double_select.py --non_pair --tides_factor {sys.argv[3]}  --halo_select {sys.argv[-1]}  --snap_base {snap_base}  {ii}"
     print(my_cmd)
     bash_command(my_cmd)
