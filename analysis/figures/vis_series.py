4b653d9c5bb6e5a86327db602d272b893b9c45a8
alekseygenerozov <aleksey.generozov@gmail.com>
2026-04-01 16:22:37 -0400

    Fixing typecasting

--- analysis/figures/vis_series.py
+++ analysis/figures/vis_series.py
@@ -48,7 +48,7 @@ def main():
     # process_example(sys.argv[1])
     ##Parallel image productions(!)
     with multiprocessing.Pool(10) as pool:
-        pool.map(process_example, range(sys.argv[1]))
+        pool.map(process_example, range(int(sys.argv[1])))
 
 
 if __name__ == "__main__":
