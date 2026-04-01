277b1c9a3e9e111d06ff0c1dce8785f26fe40c60
alekseygenerozov <aleksey.generozov@gmail.com>
2026-04-01 11:44:51 -0400

    Gearing up large series of visualizations.

--- analysis/figures/fig1_extend.py
+++ analysis/figures/fig1_extend.py
@@ -259,7 +259,7 @@ else:
     ax.set_xlim(-rmax, rmax)
     ax.set_ylim(-rmax, rmax)
 
-fig.savefig(f"fig1_{sys.argv[1]}a_{snap_idx}." + savetype, dpi=300)
+# fig.savefig(f"fig1_{sys.argv[1]}a_{snap_idx}." + savetype, dpi=300)
 ############################################################################################################
 # dist_center = partpos - center[:3]
 # dist_center = np.sum(dist_center * dist_center, axis=1)**.5
@@ -286,37 +286,37 @@ for ii in range(len(partpos_filt)):
     # Create the circular patch comparable to the accretion radius...Really this is an upper bound(!)
     # hl_radius = 2. * sfc.GN * partmasses_filt[ii] / np.linalg.norm(partvel_filt[ii])**2.
     ##Arbitrary cutoff for gas neighbors...
-    gas_neighbors = np.linalg.norm(xuniq[sel2_gas] - partpos_filt[ii], axis=1) < 0.01
-    if len(xuniq[sel2_gas][gas_neighbors]) == 0:
-        continue
-    gas_neighbors_vel = np.mean(vuniq[sel2_gas][gas_neighbors], axis=0)
-    gas_neighbors_cs = np.mean(u_to_cs(uuniq[sel2_gas][gas_neighbors]))
-    bhl_radius = (
-        2.0
-        * sfc.GN
-        * partmasses_filt[ii]
-        / (
-            np.linalg.norm(partvel_filt[ii] - gas_neighbors_vel) ** 2.0
-            + gas_neighbors_cs**2.0
-        )
-    )
-    print(
-        gas_neighbors_cs / 1e3,
-        gas_neighbors_vel / 1e3,
-        np.linalg.norm(partvel_filt[ii] - gas_neighbors_vel) / 1e3,
-        np.linalg.norm(partvel_filt[ii]) / 1e3,
-    )
+    # gas_neighbors = np.linalg.norm(xuniq[sel2_gas] - partpos_filt[ii], axis=1) < 0.01
+    # if len(xuniq[sel2_gas][gas_neighbors]) == 0:
+    #     continue
+    # gas_neighbors_vel = np.mean(vuniq[sel2_gas][gas_neighbors], axis=0)
+    # gas_neighbors_cs = np.mean(u_to_cs(uuniq[sel2_gas][gas_neighbors]))
+    # bhl_radius = (
+    #     2.0
+    #     * sfc.GN
+    #     * partmasses_filt[ii]
+    #     / (
+    #         np.linalg.norm(partvel_filt[ii] - gas_neighbors_vel) ** 2.0
+    #         + gas_neighbors_cs**2.0
+    #     )
+    # )
+    # print(
+    #     gas_neighbors_cs / 1e3,
+    #     gas_neighbors_vel / 1e3,
+    #     np.linalg.norm(partvel_filt[ii] - gas_neighbors_vel) / 1e3,
+    #     np.linalg.norm(partvel_filt[ii]) / 1e3,
+    # )
 
-    circle = patches.Circle(
-        (center_x, center_y),
-        bhl_radius,
-        color="orange",
-        fill=False,
-        linewidth=2,
-        label="Circle",
-    )
-    # Add the circle to the axes
-    ax.add_patch(circle)
+    # circle = patches.Circle(
+    #     (center_x, center_y),
+    #     bhl_radius,
+    #     color="orange",
+    #     fill=False,
+    #     linewidth=2,
+    #     label="Circle",
+    # )
+    # # Add the circle to the axes
+    # ax.add_patch(circle)
 arr_index1 = np.where(partids_filt.astype(int) == bin_id1)[0]
 arr_index2 = np.where(partids_filt.astype(int) == bin_id2)[0]
 ax.plot(
@@ -331,7 +331,7 @@ ax.plot(
 )
 
 
-fig.savefig(f"fig1_{sys.argv[1]}b_{snap_idx}." + savetype, dpi=300)
+# fig.savefig(f"fig1_{sys.argv[1]}b_{snap_idx}." + savetype, dpi=300)
 
 # prop_cycle = plt.rcParams['axes.prop_cycle']
 # colors = prop_cycle.by_key()['color']
---
+++ analysis/figures/vis_series.py
@@ -0,0 +1,47 @@
+import multiprocessing
+import os
+import subprocess
+import sys
+
+import numpy as np
+
+
+def bash_command(cmd, **kwargs):
+    """Run command from the bash shell"""
+    process = subprocess.Popen(["/bin/bash", "-c", cmd], **kwargs)
+    return process.communicate()[0]
+
+
+def process_example(ii):
+    os.chdir(f"example_{ii}")
+    times = np.genfromtxt("times")
+
+    with open("config_template", "r") as ff:
+        config_template = ff.read()
+
+    with open("config_template_b", "r") as ff:
+        config_template_b = ff.read()
+
+    for ss in range(times[0], times[1]):
+        config = config_template.replace("SS", str(ss))
+        with open("config_0", "w") as fout:
+            fout.write(config)
+        bash_command(
+            "python3 ../starforge_mult_search/analysis/figures/fig1_extend.py 0"
+        )
+
+    for ss in range(times[1], times[2] + 1):
+        config = config_template_b.replace("SS", str(ss))
+        with open("config_0", "w") as fout:
+            fout.write(config)
+        bash_command(
+            "python3 ../starforge_mult_search/analysis/figures/fig1_extend.py 0"
+        )
+
+
+def main():
+    process_example(sys.argv[1])
+    ##Parallel image productions(!)
+    # with multiprocessing.Pool(10) as pool:
+    #     pool.map(process_example, range(sys.argv[1]))
+    #     pool.map(process_example, range(sys.argv[1]))
